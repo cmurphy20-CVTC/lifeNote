cb09d63c7088ce4f5a95a1b2758be93eeeb65347
Casey <89794911+cmurphy20-CVTC@users.noreply.github.com>
2022-07-14 21:32:16 -0500

    [(Finishes| Delivers) #182706954 #182683408 #182728547 users can save to db

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const findOrCreate = require('mongoose-findorcreate');
-const FacebookStrategy = require('passport-facebook').Strategy;
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -34,10 +33,10 @@ app.use(passport.session());
 
 mongoose.connect(process.env.DB_LINK, {useNewUrlParser: true});
 
-
 const postSchema = new mongoose.Schema ({
 
-
+  userId: String,
+  title: String,
   content: String
 
 });
@@ -92,26 +91,6 @@ function(accessToken, refreshToken, profile, cb) {
 }
 ));
 
-// Facebook Strategy
-// passport.use(new FacebookStrategy({
-//   clientID: process.env.FACEBOOK_APP_ID,
-//   clientSecret: process.env.FACEBOOK_APP_SECRET,
-//   callbackURL: "http://localhost:3000/auth/facebook/post",
-//   enableProof: true,
-//   profileFields: ["id", "email"]
-// },
-// function(accessToken, refreshToken, profile, cb) {
-//   User.findOrCreate(
-//     { username: profile.id },
-//     { 
-//       provider: "google",
-//       email: profile._json.email
-//     }, function (err, user) {
-//       return cb(err, user);
-//   });
-// }
-// ));
-
 app.get("/auth/google", passport.authenticate('google', {
   
   scope: ["profile", "email"]
@@ -124,18 +103,6 @@ app.get('/auth/google/post',
     // Successful authentication, redirect home.
     res.redirect('/post');
   });
-
-//   app.get('/auth/facebook',
-//   passport.authenticate('facebook', {
-//     scope: ["email"]
-//   }));
-
-// app.get('/auth/facebook/post',
-//   passport.authenticate('facebook', { failureRedirect: '/login' }),
-//   function(req, res) {
-//     // Successful authentication, redirect home.
-//     res.redirect('/post');
-//    });
 
 app.get("/", function(req, res){
   
@@ -155,11 +122,22 @@ app.get("/register", function(req, res){
 
 app.get("/post", function(req, res){
 
-  if(req.isAuthenticated()) {
-    res.render("post")
-  } else {
-    res.redirect("/login")
-  }
+  User.findById(req.user.id, function(err, userPosts){
+    if (err){
+      console.log(err);
+    } else {
+      if (userPosts) {
+       
+        res.render("post", {postsForPage: userPosts.posts});
+      }
+    }
+  });
+
+  // if(req.isAuthenticated()) {
+  //   res.render("post")
+  // } else {
+  //   res.redirect("/login")
+  // }
 })
 
 
@@ -194,33 +172,31 @@ app.get("/logout", function(req, res) {
 
 app.post("/compose", function(req, res){
 
-  // const yourPostTitle = req.body.title;
-   
+  const yourPostId = req.user.email;
+  const yourPostTitle = req.body.postTitle;
+  const yourPostContent = req.body.postBody; 
   
-  // const newPost = {
-  //   content: yourPostContent
-  // };
-if (req.isAuthenticated) {
+  const newPost = new Post ({
+    userId: yourPostId,
+    title: yourPostTitle,
+    content: yourPostContent
 
-  // userSchema.findOneAndUpdate({_id: req.user.id}, {$addToSet : {
-  //   posts: yourPostContent
-  // }})
-
-  // res.redirect("/post")
+  });
+//if (req.isAuthenticated) {
 
   User.findById(req.user.id, function(err, userFound){
     if (err) {
       console.log(err)
     } else {
       if(userFound) {
-        userFound.posts.push(req.body.content);
+        userFound.posts.push(newPost);
         userFound.save(function(){
           res.redirect('/post')
         })
       }
     }
   })
-}
+//}
 });
 
 // app.get("/posts/:postId", function(req, res){
@@ -246,8 +222,8 @@ app.post("/register", function(req, res){
     username: req.body.username, 
     firstName: req.body.firstName, 
     lastName: req.body.lastName, 
-    email: req.body.email
-    }), req.body.password,
+    email: req.body.email,
+    posts: "Test"}), req.body.password,
      function(err, user){
 
     if (err) {
@@ -284,8 +260,6 @@ app.post("/login", function(req, res){
       passport.authenticate("local")(req, res, function(){
         res.redirect("/post");
       });
-
-      
 
     }
   })
