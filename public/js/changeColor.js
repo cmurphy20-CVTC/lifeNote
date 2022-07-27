9043a25f308a6ecdf2361a2903261a3ee67acfd2
Casey <89794911+cmurphy20-CVTC@users.noreply.github.com>
2022-07-26 20:38:46 -0500

    [(Finishes|Delivers) #182683270] user can switch between colors

--- public/js/changeColor.js
+++ public/js/changeColor.js
@@ -1,8 +1,32 @@
 $(function() {
 
-  $colorPicked = $("#forBlue");
+  $blue = $("#forBlue");
+  $red = $("#forRed");
+  $default = $("#forDefault");
+  $purple = $("#forPurple");
 
-  $colorPicked.on("click", function() {
-    console.log("Blue")
+  $red.on("click", function() {
+    $(".footer").css("background-color",  "#C70039");
+
+    $(".navbar").css("background-color", "#C70039")
   })
+
+  $blue.on("click", function() {
+    $(".footer").css("background-color",  "#0000FF ");
+
+    $(".navbar").css("background-color", "#0000FF")
+  })
+
+  $purple.on("click", function() {
+    $(".footer").css("background-color",  "#8F00FF ");
+
+    $(".navbar").css("background-color", "#8F00FF")
+  })
+
+  $default.on("click", function() {
+    $(".footer").css("background-color",  "#65d842");
+
+    $(".navbar").css("background-color", "#65d842")
+  })
+
 })
