3a03063f730a3ed205642fb3d7c701e1e307ffe5
Casey <89794911+cmurphy20-CVTC@users.noreply.github.com>
2022-11-02 20:50:24 -0500

    [(Finishes|Deilvers) #183705375] custom animations added

---
+++ public/js/formAnimations.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+  // Animate for Contact Form
+  $contactForm = $(".animateForm");
+
+  $contactForm.hide().show(1500);
+
+  // Animate for About and Features pages
+
+  $featureP =$('.feature-paragraph > p');
+
+  $featureP.css({
+     opacity: 0,
+  }).fadeTo( 1250, 1);
+
+// Register and Login Froms
+
+  $regLogForm = $('.regLogForm');
+
+  $regLogForm.css({
+    right: 1000,
+    opacity: 0
+  }).animate({
+    right: -10,
+    opacity: 1
+  }, 750);
+
+  
+ 
+});
