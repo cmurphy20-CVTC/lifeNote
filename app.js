caf5fc4dad9a04b9b934f80d7972254cfb0ea8d9
Casey <89794911+cmurphy20-CVTC@users.noreply.github.com>
2022-11-05 09:26:58 -0500

    fixed app.listen

--- app.js
+++ app.js
@@ -457,9 +457,7 @@ if(error.status === 500) {
 
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-app.listen(port);
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
 
