b076e1a3da75f5385ca3b0c44ce04a544ba29b78
Casey <89794911+cmurphy20-CVTC@users.noreply.github.com>
2022-11-05 10:01:00 -0500

    refactor profile and port

--- app.js
+++ app.js
@@ -457,7 +457,12 @@ if(error.status === 500) {
 
 });
 
-app.listen(`0.0.0.0:$PORT`, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
   console.log("Server has started successfully ");
 });
 
