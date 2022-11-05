5240981fab531f785d8cf245e427b8deff37c926
Casey <89794911+cmurphy20-CVTC@users.noreply.github.com>
2022-11-05 09:29:50 -0500

    added railway port number

--- app.js
+++ app.js
@@ -457,7 +457,7 @@ if(error.status === 500) {
 
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(`0.0.0.0:$PORT`, function() {
+  console.log("Server has started successfully ");
 });
 
